2c5102b902ac65386964a0e883f39a779e2f0c6d
Reece H. Dunn <msclrhd@gmail.com>
2016-04-23 13:37:18 +0100

    Rename SkosValidator to TagsetValidator.

--- cmudicttools/cmudict.py
+++ cmudicttools/cmudict.py
@@ -90,7 +90,7 @@ class TypeValidator:
 		except ValueError:
 			return False, value
 
-def SkosValidator(path, schemeName=None):
+def TagsetValidator(path, schemeName=None):
 	for key, values in metadata.parse(path).items():
 		if key == schemeName or not schemeName:
 			return SetValidator(values)
@@ -294,7 +294,7 @@ dict_formats = { # {0} = word ; {1} = context ; {2} = phonemes ; {3} = comment
 		'word': lambda word: word.upper(),
 		# parsing:
 		'word-validation': r'^[^ a-zA-Z]?[A-Z0-9\'\.\-\_\x80-\xFF]*$',
-		'context-parser': SkosValidator(os.path.join(root, 'pos-tags', 'cmu.ttl'), 'cmu'),
+		'context-parser': TagsetValidator(os.path.join(root, 'pos-tags', 'cmu.ttl'), 'cmu'),
 	},
 	'cmudict': {
 		'accent': 'en-US',
@@ -309,7 +309,7 @@ dict_formats = { # {0} = word ; {1} = context ; {2} = phonemes ; {3} = comment
 		'word': lambda word: word.upper(),
 		# parsing:
 		'word-validation': r'^[^ a-zA-Z]?[A-Z0-9\'\.\-\_\x80-\xFF]*$',
-		'context-parser': SkosValidator(os.path.join(root, 'pos-tags', 'cmu.ttl'), 'cmu'),
+		'context-parser': TagsetValidator(os.path.join(root, 'pos-tags', 'cmu.ttl'), 'cmu'),
 	},
 	'cmudict-new': {
 		'accent': 'en-US',
@@ -324,7 +324,7 @@ dict_formats = { # {0} = word ; {1} = context ; {2} = phonemes ; {3} = comment
 		'word': lambda word: word.lower(),
 		# parsing:
 		'word-validation': r'^[^ a-zA-Z]?[a-z0-9\'\.\-\_\x80-\xFF]*$',
-		'context-parser': SkosValidator(os.path.join(root, 'pos-tags', 'cmu.ttl'), 'cmu'),
+		'context-parser': TagsetValidator(os.path.join(root, 'pos-tags', 'cmu.ttl'), 'cmu'),
 	},
 	'festlex': {
 		'accent': 'en-US',
@@ -339,7 +339,7 @@ dict_formats = { # {0} = word ; {1} = context ; {2} = phonemes ; {3} = comment
 		'word': lambda word: word.lower(),
 		# parsing:
 		'word-validation': r'^[^ a-zA-Z]?[a-z0-9\'\.\-\_\x80-\xFF]*$',
-		'context-parser': SkosValidator(os.path.join(root, 'pos-tags', 'festlex.ttl'), 'festlex'),
+		'context-parser': TagsetValidator(os.path.join(root, 'pos-tags', 'festlex.ttl'), 'festlex'),
 	},
 	'sphinx': {
 		'accent': 'en-US',
@@ -948,7 +948,7 @@ def parse(filename, warnings=[], order_from=0, accent=None, phoneset=None, encod
 						path = os.path.join(os.path.dirname(filename), entry)
 						if not os.path.exists(path):
 							path = os.path.join(root, 'pos-tags', '{0}.ttl'.format(entry))
-						context_parser = SkosValidator(path)
+						context_parser = TagsetValidator(path)
 			continue
 
 		if not fmt:
